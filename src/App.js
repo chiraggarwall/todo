91a2c3bc4344bc888932cdb658faa4c447960e8a
Chirag <chiragaggarwal51@yahoo.com>
2024-10-17 01:55:41 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,122 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+
+  function updateText(id, inputText) {
+    let updateList = todoList.map((e) => {
+      if (e.id === id) {
+        e.text = inputText;
+      }
+      return e;
+    });
+
+    setTodoList(updateList);
+  }
+  function handleDelete(id) {
+    console.log(id);
+    const updateList = todoList.filter((e) => {
+      return e.id !== id;
+    });
+
+    setTodoList(updateList);
+  }
+
+  function handleComplete(id) {
+    let updateList = todoList.map((e) => {
+      if (e.id === id) {
+        e.complete = !e.complete;
+      }
+      return e;
+    });
+
+    setTodoList(updateList);
+  }
+  function handleOnKeyDown(e) {
+    if (e.key === "Enter") {
+      setTodoList([
+        ...todoList,
+        { text: e.target.value, completed: false, id: Date.now() },
+      ]);
+      e.target.value = "";
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" onKeyDown={handleOnKeyDown} />
+
+      {todoList.map((e) => {
+        return (
+          <>
+            <Item
+              key={e.id}
+              text={e.text}
+              id={e.id}
+              complete={e.complete}
+              handleComplete={handleComplete}
+              handleDelete={handleDelete}
+              updateText={updateText}
+            />
+          </>
+        );
+      })}
     </div>
   );
 }
+
+const Item = ({
+  text,
+  handleComplete,
+  complete,
+  id,
+  handleDelete,
+  updateText,
+}) => {
+  const [editText, setEditText] = useState(false);
+  const [inputText, setText] = useState(text);
+  console.log(editText);
+  return (
+    <main className="item">
+      <div
+        className="complete"
+        onClick={() => {
+          handleComplete(id);
+        }}
+      >
+        {complete ? <span>✓</span> : ""}
+      </div>
+      <div
+        onDoubleClick={() => {
+          setEditText(true);
+        }}
+        className={complete ? "textOver" : ""}
+      >
+        {editText ? (
+          <input
+            value={inputText}
+            onChange={(e) => {
+              setText(e.target.value);
+            }}
+            onBlur={() => {
+              setEditText(false);
+              updateText(id, inputText);
+            }}
+          />
+        ) : (
+          inputText
+        )}
+      </div>
+      <div
+        className="close"
+        onClick={() => {
+          handleDelete(id);
+        }}
+      >
+        X
+      </div>
+    </main>
+  );
+};
 
 export default App;
